05ae6e3c5f2ac80b181523cc38c4f29c76d6fe14
dungtri2803 <nguyentridung2803@gmail.com>
2022-03-26 23:46:33 +0700

    update news, news-detail, pay

--- js/sldier.js
+++ js/sldier.js
@@ -230,3 +230,42 @@ var swiper = new Swiper(".yard-detail-similar", {
       },
   }
 });
+// news-detail-similar
+var swiper = new Swiper(".news-detail", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+      280:{
+          slidesPerView: 1,
+          spaceBetween: 10,
+      },
+      320:{
+          slidesPerView: 1,
+          spaceBetween: 10,
+      },
+      510:{
+          slidesPerView: 1,
+          spaceBetween: 10,
+      },
+      758:{
+          slidesPerView: 2,
+          spaceBetween: 15,
+      },
+      900:{
+          slidesPerView: 3,
+          spaceBetween: 20,
+      },
+  }
+});
